af1a6ba0475c2a101647ae10545f867a32cd9f76
denrsch <you@exam>
2015-04-02 20:09:23 -0400

    rectangle equation added

--- Circle_calculations.py
+++ Circle_calculations.py
@@ -1,7 +1,21 @@
 #calculating the area of a circle with input requests
 
 import math 
-radius_input = input("What size is the radius?" )
+radius_input = input("What size is the radius? ")
 r = int(radius_input)
 
-circle = math.p*r*r
+circle = math.pi * r * r
+
+print "The area of the circle is %s" % (circle)
+
+#calculating the area of a rectangle with a user prompt
+
+width_input = input("how wide is your rectangle? ")
+width = int(width_input)
+
+length_input = input("how long is your rectangle? ")
+length = int(length_input)
+
+rectangle = length * width
+
+print "looks like the area of your rectangle is %s" % (rectangle)
